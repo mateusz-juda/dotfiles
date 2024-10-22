8559014eeac22f2bf536d0b7fd8a12fcded18786
Mateusz <me@tom.local>
2024-10-22 23:40:24 +0200

    .

--- dot_mine/run_once_link_zshrcd.sh
+++ dot_mine/run_once_link_zshrcd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mkdir -p $PWD/.zshrc.d
-ln -s $PWD/common_sh $PWD/.zshrc.d/00_common_sh
+mkdir -p .zshrc.d
+ln -s $PWD/common_sh .zshrc.d/00_common_sh
--- run_once.sh
+++ run_once.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
 
-mkdir -p $PWD/.zshrc.d
-
-if [ "$PWD" != "$HOME" ]; then
-    ln -s $PWD/.zshrc.d $HOME/.zshrc.d
-    ln -s $PWD/.zshrc $HOME/.zshrc
-fi
