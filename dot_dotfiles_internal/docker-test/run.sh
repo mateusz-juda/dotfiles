1856ffd10f15f4f81b91c648ef2497b51c9ee192
Mateusz <me@tom.local>
2024-10-22 22:49:11 +0200

    common_sh

--- dot_dotfiles_internal/docker-test/run.sh
+++ dot_dotfiles_internal/docker-test/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WD="/Users/me/work/mine/"
-echo "Run: chezmoi init --guess-repo-url=false $WD/dotfiles"
+echo "Run: chezmoi init --guess-repo-url=false $WD/dotfiles && chezmoi apply"
 docker run -ti -v $WD:$WD -w /tmp/home -u $(id -u):$(id -g) chezmoi-test
 
 
---
+++ dot_mine/run_once_link_zshrcd.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p $HOME/.zshrc.d
+ln -s common_sh $HOME/.zshrc.d/00_common_sh
